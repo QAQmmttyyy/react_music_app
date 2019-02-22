825a77d322b15068da848f6a7feda7eed38c9018
QAQmmttyyy <miaotianyugo@163.com>
2019-02-22 16:03:15 +0800

    Updates

---
+++ precache-manifest.dfe017afe366ca38bf089ea28ffa3e3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f22f49db8dc284545b0c4e4f33f3204",
+    "url": "/react_music_app/static/media/playall.8f22f49d.png"
+  },
+  {
+    "revision": "d45643b2f98dbb321312f552e2ed88a3",
+    "url": "/react_music_app/static/media/cover-placeholder.d45643b2.png"
+  },
+  {
+    "revision": "58535fe6429c752f205b",
+    "url": "/react_music_app/static/js/runtime~main.58535fe6.js"
+  },
+  {
+    "revision": "f7b96e16d15480e282e3",
+    "url": "/react_music_app/static/js/main.f7b96e16.chunk.js"
+  },
+  {
+    "revision": "ca9b14fd3e830a0ff704",
+    "url": "/react_music_app/static/js/1.ca9b14fd.chunk.js"
+  },
+  {
+    "revision": "f7b96e16d15480e282e3",
+    "url": "/react_music_app/static/css/main.243333c1.chunk.css"
+  },
+  {
+    "revision": "ca9b14fd3e830a0ff704",
+    "url": "/react_music_app/static/css/1.3bf7771e.chunk.css"
+  },
+  {
+    "revision": "fe299bc05b6fa173785bf081743d0af5",
+    "url": "/react_music_app/index.html"
+  }
+];
